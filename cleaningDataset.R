a4a00c3f66a6b5f26870886115fec7b3477862ea
Juan Sebastián Beleño Díaz <jsbeleno@gmail.com>
2016-04-03 16:48:32 -0300

    Advances in getting data from imdb database, but still it's needed the director information in the data

--- cleaningDataset.R
+++ cleaningDataset.R
@@ -1,20 +1,83 @@
 # Pre-processing the movie information to avoid problems with comma
+# And adding some extra data
+
+# Load library
+library(httr)
 
 # Defining the file name
 fileName <- "./data/movie_titles.txt"
 fileNameCleaned <- "./data/movie_titles_clean.txt"
+fileNameActors <- "./data/movie_with_actors.txt"
 
 # Loading one line per iteration, changing the value and writting the
 # result in a new file, source = http://stackoverflow.com/a/28281908
 con  <- file(fileName, open = "r")
 while (length(oneLine <- readLines(con, n = 1, warn = FALSE)) > 0) {
+    
+    # Wait 100ms
+    Sys.sleep(0.1)
+    
+    # Using gsub to separate the title from other data per movie
     movie_title <- gsub("^([^,]*,[^,]*),", "", oneLine)
     movie_data_without_title <- sub("^([^,]*,[^,]*,).*", "\\1", oneLine)
     
+    # Replacing comma for semicolon as separator
     movie_data_without_title <- gsub(",", ";", movie_data_without_title)
     
-    movie_data <- paste0(movie_data_without_title, movie_title)
     
-    write(movie_data, file = fileNameCleaned, append = TRUE)
+    
+    # Adding data about genre, language and country in the movie
+    
+    # Request execution
+    requestMovieData <- GET(
+        "http://www.omdbapi.com/?", 
+        query = list(t = movie_title)
+    )
+    
+    # Getting the content in json format
+    movieDataWeb <- content(requestMovieData, "parsed", encoding = "UTF-8")
+    
+    if(requestMovieData$status_code == 200){
+        if(movieDataWeb$Response == "True"){
+        
+            # Join all the data using semicolon
+            movie_data <- paste0(movie_data_without_title, 
+                                 movie_title, ";", 
+                                 movieDataWeb$Genre, ";",
+                                 movieDataWeb$Language, ";",
+                                 movieDataWeb$Country)
+            
+            # Writting the final data in a file
+            write(movie_data, file = fileNameCleaned, append = TRUE)
+            
+            
+            # Getting actors data and parsing it
+            if(movieDataWeb$Actors != "N/A"){
+                actors <- strsplit(movieDataWeb$Actors, ",")[[1]]
+                
+                # For loop with actors
+                if(length(actors) > 0){
+                    for(i in 1:length(actors)){
+                        movie_data2 <- paste0(movie_data, ";", trimws(actors[i]))
+                        
+                        # Writting the final data in a different file
+                        write(movie_data2, file = fileNameActors, append = TRUE)
+                    }
+                }
+            }
+        
+        }else{
+            # Join all the data using semicolon
+            movie_data <- paste0(movie_data_without_title, 
+                                 movie_title, ";", 
+                                 "NULL;",
+                                 "NULL;",
+                                 "NULL")
+            
+            # Writting the final data in a file
+            write(movie_data, file = fileNameCleaned, append = TRUE)
+        }
+    }
+    
 } 
 close(con)
--- exploratoryAnalysis.R
+++ exploratoryAnalysis.R
@@ -3,20 +3,33 @@
 # Load libraries
 library(ggplot2)
 
+# Defining the file name
+fileNameCleaned <- "./data/movie_titles_clean.txt"
+fileNameActors <- "./data/movie_with_actors.txt"
+
 # Reading movie information
-movies <- read.csv2(fileNameCleaned, header = FALSE, col.names = c("id", "year", "title"), 
-                    colClasses = c("integer", "character", "character"), comment.char = "",
-                    na.strings = "NA")
+movies <- read.csv2(fileNameCleaned, 
+                    header = FALSE, col.names = c("id", "year", "title", "genre", "language", "country"), 
+                    colClasses = c("integer", "character", "character", "character", "character", "character"), 
+                    comment.char = "", na.strings = "NA")
 
 # Some movies has a NULL year so it's important to convert this data in integer
 # omitting the NA values
 movies$year <- as.integer(movies$year)
 
 # Histogram of movies per year
-qplot(movies$year, geom="histogram", binwidth = 1,  
-      main = "Histogram for year of the movie", 
+qplot(movies$year, 
+      geom="histogram", 
+      binwidth = 1,  
+      main = "Histogram of movies per year", 
       xlab = "Year",  
       fill=I("blue"), 
       col=I("red"), 
       alpha=I(.2),
       xlim=c(1890,2009))
+
+# Reading movie data with actors
+actors <- read.csv2(fileNameActors, 
+                    header = FALSE, col.names = c("id", "year", "title", "genre", "language", "country", "actor"), 
+                    colClasses = c("integer", "character", "character", "character", "character", "character", "character"), 
+                    comment.char = "", na.strings = "NA")
